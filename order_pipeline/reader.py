b81d0a23206c22ddfb6d8cc32e13883b6d012474
Hezekiah Ajayi-Omoleye <hezekiahomoleye@gmail.com>
2025-10-25 15:58:25 +0100

    Reader

--- order_pipeline/reader.py
+++ order_pipeline/reader.py
@@ -1,5 +1,42 @@
 import json
-with open("shoplink.json", "r") as f:   # so open shoplink as readable and stire it in f
-    shoplink = json.load(f)
+import os
 
-print(shoplink)
+
+def load_json_list(shoplink: str):
+
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+    file_path = os.path.join(BASE_DIR,"shoplink.json")
+
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"File was not found at {file_path}")
+    
+    if os.path.getsize(file_path) == 0:
+        raise ValueError("The file is empty")
+    
+    with open(file_path, "r", encoding="utf-8") as f:
+        try:
+            shoplink = json.load(f)
+
+        except json.JSONDecodeError as e:
+            raise ValueError(f"Invalid JSON format: {e}")
+        
+
+
+    if not isinstance(shoplink, list) or not all(isinstance(i, dict) for i in shoplink):
+        raise ValueError(f"Expected a list of dictionaries in the JSON file. {file_path}")
+    
+    return shoplink
+
+
+try: 
+    shoplink = load_json_list("shoplink.json")
+
+    print("\n✅ File loaded successfully!\nItems in shoplink.json:\n")
+    
+    for item in shoplink:
+        print(item)
+
+# Catch and print errors if something goes wrong (like missing file or bad JSON)
+except (FileNotFoundError, ValueError) as e:
+    print("Error:", e)
